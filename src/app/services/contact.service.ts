deab3e1679a278e120c2c60c1d35ad2a89b9cf53
James Dora <jamesdoraiii@gmail.com>
2019-05-07 21:01:06 -0400

    fix

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':'application/json'
-  })
-}
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type':'application/json'
+//   })
+// }
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
 
-export class ContactService {
+// export class ContactService {
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
-createContact(): Observable<any[]>{
-  return this.http.post<any[]>(httpOptions)
-}
+// createContact(): Observable<any[]>{
+//   return this.http.post<any[]>(httpOptions)
+// }
 
-findUserContacts(): Observable<any>{
-  return this.http.get<any>()
-}
+// findUserContacts(): Observable<any>{
+//   return this.http.get<any>()
+// }
 
-findDetailerContacts(): Observable<any>{
-  return this.http.get<any>()
-}
+// findDetailerContacts(): Observable<any>{
+//   return this.http.get<any>()
+// }
 
-}
+// }
--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,35 +1,35 @@
-import { Injectable } from '@angular/core';
-//import { HttpClient, HttpHeaders } from 'angular/common/http';
-//import { Review } from '../models/review.model';
-import { Observable } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpClient } from 'selenium-webdriver/http';
+// import { Injectable } from '@angular/core';
+// //import { HttpClient, HttpHeaders } from 'angular/common/http';
+// //import { Review } from '../models/review.model';
+// import { Observable } from 'rxjs';
+// import { HttpHeaders } from '@angular/common/http';
+// import { HttpClient } from 'selenium-webdriver/http';
 
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type' : 'application/json'
-  })
-}
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type' : 'application/json'
+//   })
+// }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ReviewService {
-  private reviewUrl = 'http://localhost:3000/'
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ReviewService {
+//   private reviewUrl = 'http://localhost:3000/'
 
-  constructor(private http: HttpClient) { }
-
-
-createReview(Review): Observable<any>{
-  return this.http.post<any[]>()
-}
-
-getReview(): Observable<any[]>{
-  return this.http.get<any[]>(review, httpOptions)
-}
+//   constructor(private http: HttpClient) { }
 
 
+// createReview(Review): Observable<any>{
+//   return this.http.post<any[]>()
+// }
+
+// getReview(): Observable<any[]>{
+//   return this.http.get<any[]>(review, httpOptions)
+// }
 
 
-}
+
+
+// }
